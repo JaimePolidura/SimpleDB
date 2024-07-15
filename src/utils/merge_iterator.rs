a487821e65ed30ef53f12faa3ae2f891e3c070d8
Jaime Polidura <jaime.polidura@gmail.com>
2024-07-15 19:42:28 +0200

    Fixed some merge iterator bugs

--- src/utils/merge_iterator.rs
+++ src/utils/merge_iterator.rs
@@ -6,11 +6,15 @@ pub struct MergeIterator<I: StorageIterator> {
 
     current_iterator: Option<Box<I>>,
     current_iterator_index: usize,
+    last_key_iterated: Key,
 }
 
 impl<I: StorageIterator> MergeIterator<I> {
-    pub fn new(iterators: Vec<Box<I>>) -> MergeIterator<I> {
+    pub fn new(mut iterators: Vec<Box<I>>) -> MergeIterator<I> {
+        call_next_on_every_iterator(&mut iterators);
+
         MergeIterator {
+            last_key_iterated: Key::default(),
             current_iterator_index: 0,
             current_iterator: None,
             iterators,
@@ -18,25 +22,40 @@ impl<I: StorageIterator> MergeIterator<I> {
     }
 }
 
+fn call_next_on_every_iterator<I: StorageIterator>(iterators: &mut Vec<Box<I>>) {
+    for iterator in iterators {
+        iterator.next();
+    }
+}
+
 impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
     fn next(&mut self) -> bool {
-        self.iterators.insert(self.current_iterator_index, self.current_iterator.take().unwrap());
+        if self.current_iterator.is_some() {
+            self.current_iterator.as_mut().unwrap().next();
+            self.iterators.insert(self.current_iterator_index, self.current_iterator.take().unwrap());
+        }
 
-        let mut min_key_seen: Option<&Key> = None;
+        let mut min_key_seen: Option<Key> = None;
         let mut min_key_seen_index: usize = 0;
         let mut current_index: i32 = -1;
-        for iterator in &self.iterators {
+        for current_iterator in &mut self.iterators {
             current_index = current_index + 1;
 
-            if iterator.has_next() {
-                let current_key = iterator.key();
+            if current_iterator.has_next() {
+                let current_key = current_iterator.key();
 
-                match min_key_seen {
-                    Some(_) => if current_key.le(min_key_seen.unwrap()) {
-                        min_key_seen_index = current_index as usize;
-                        min_key_seen = Some(current_key);
-                    },
-                    None => min_key_seen = Some(current_key),
+                if current_key > &self.last_key_iterated {
+                    match min_key_seen {
+                        Some(_) => if current_key.le(min_key_seen.as_ref().unwrap()) {
+                            min_key_seen_index = current_index as usize;
+                            min_key_seen = Some(current_key.clone());
+                        },
+                        None => min_key_seen = Some(current_key.clone()),
+                    }
+                }
+
+                if current_key <= &self.last_key_iterated {
+                    current_iterator.next();
                 }
             }
         }
@@ -45,6 +64,7 @@ impl<I: StorageIterator> StorageIterator for MergeIterator<I> {
 
         if some_key_found {
             self.current_iterator = Some(self.iterators.remove(min_key_seen_index));
+            self.last_key_iterated = self.current_iterator.as_ref().unwrap().key().clone();
         }
 
         some_key_found
