556ad231f41f8a75347432d72bd4badcd86509d4
Jaime Polidura <jaime.polidura@gmail.com>
2024-08-09 13:46:16 +0200

    changed gitignore

--- core/target/debug/build/serde_json-5dd976df2c47316b/build_script_build-5dd976df2c47316b.d
+++
@@ -1,5 +0,0 @@
-C:\programacion\mini-lsm\core\target\debug\build\serde_json-5dd976df2c47316b\build_script_build-5dd976df2c47316b.exe: C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.122\build.rs
-
-C:\programacion\mini-lsm\core\target\debug\build\serde_json-5dd976df2c47316b\build_script_build-5dd976df2c47316b.d: C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.122\build.rs
-
-C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\serde_json-1.0.122\build.rs:
--- core/target/debug/deps/either-651b7af77511ecef.d
+++
@@ -1,9 +0,0 @@
-C:\programacion\mini-lsm\core\target\debug\deps\libeither-651b7af77511ecef.rmeta: C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\lib.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\iterator.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\into_either.rs
-
-C:\programacion\mini-lsm\core\target\debug\deps\libeither-651b7af77511ecef.rlib: C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\lib.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\iterator.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\into_either.rs
-
-C:\programacion\mini-lsm\core\target\debug\deps\either-651b7af77511ecef.d: C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\lib.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\iterator.rs C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\into_either.rs
-
-C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\lib.rs:
-C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\iterator.rs:
-C:\Users\jaime\.cargo\registry\src\index.crates.io-6f17d22bba15001f\either-1.13.0\src\into_either.rs:
