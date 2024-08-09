9d105323d520da1f9c5679a0bdafacd770011d0c
Jaime Polidura <jaime.polidura@gmail.com>
2024-08-09 13:24:04 +0200

    Implemented missing functions in lsm file

--- src/utils/lsm_file.rs
+++ src/utils/lsm_file.rs
@@ -1,12 +1,12 @@
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::os::windows::fs::FileExt;
 use std::path::{Path, PathBuf};
 
 pub struct LsmFile {
     file: Option<File>,
     path: Option<PathBuf>,
-    size: usize
+    size_bytes: usize
 }
 
 impl LsmFile {
@@ -14,16 +14,30 @@ impl LsmFile {
         LsmFile {
             path: None,
             file: None,
-            size: 0
+            size_bytes: 0
         }
     }
 
     pub fn read_all(&self) -> Result<Vec<u8>, ()> {
-        unimplemented!();
+        let mut buff: Vec<u8> = Vec::with_capacity(self.size_bytes);
+        self.file
+            .as_ref()
+            .unwrap()
+            .read_to_end(&mut buff)
+            .map_err(|e| ())?;
+
+        Ok(buff)
     }
 
     pub fn open(path: &Path) -> Result<LsmFile, ()> {
-        unimplemented!();
+        let file = File::open(path).map_err(|e| ())?;
+        let metadata = file.metadata().map_err(|e| ())?;
+
+        Ok(LsmFile{
+            file: Some(file),
+            path: Some(path.to_path_buf()),
+            size_bytes: metadata.len() as usize
+        })
     }
 
     pub fn create(
@@ -33,7 +47,7 @@ impl LsmFile {
         std::fs::write(path, data).expect("Cannot create file");
 
         match File::open(path) {
-            Ok(file) => Ok(LsmFile { size: data.len(), file: Some(file), path: Some(path.to_path_buf()) }),
+            Ok(file) => Ok(LsmFile { size_bytes: data.len(), file: Some(file), path: Some(path.to_path_buf()) }),
             Err(_) => Err(())
         }
     }
@@ -48,7 +62,7 @@ impl LsmFile {
     }
 
     pub fn size(&self) -> usize {
-        self.size
+        self.size_bytes
     }
 
     pub fn fsync(&self) {
