105c98c3f3b1084a0fadefee0598c858691482b9
Jaime Polidura <jaime.polidura@gmail.com>
2024-08-15 11:14:38 +0200

    Added LsmError message formatting

--- core/src/lsm_error.rs
+++ core/src/lsm_error.rs
@@ -1,5 +1,5 @@
 use crate::manifest::manifest::ManifestOperationContent;
-use std::fmt::{Debug, Formatter};
+use std::fmt::{format, Debug, Formatter};
 use std::string::FromUtf8Error;
 use std::path::PathBuf;
 
@@ -18,6 +18,7 @@ pub struct DecodeErrorInfo {
     pub error_type: DecodeErrorType,
 }
 
+#[derive(Copy, Clone)]
 pub enum SSTableCorruptedPart {
     BlockMetadata,
     BloomFilter,
@@ -53,6 +54,101 @@ pub enum LsmError {
 
 impl Debug for LsmError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        unimplemented!();
+        match self {
+            LsmError::CannotCreateWal(memtable_id, io_error) => {
+                write!(f, "Cannot create WAL file. Memtable ID: {}. IO Error: {}", memtable_id, io_error)
+            }
+            LsmError::CannotWriteWalEntry(memtable_id, io_error) => {
+                write!(f, "Cannot write WAL entry. Memtable ID: {}. IO Error: {}", memtable_id, io_error)
+            }
+            LsmError::CannotReadWalEntries(memtable_id, io_error) => {
+                write!(f, "Cannot read WAL entries. Memtable ID: {}. IO Error: {}", memtable_id, io_error)
+            }
+            LsmError::CannotReadWalFiles(io_error) => {
+                write!(f, "Cannot list WAL files in base path. IO Error: {}", io_error)
+            }
+            LsmError::CannotDecodeWal(memtable_id, decode_error) => {
+                write!(f, "Cannot decode WAL. Memtable ID: {} Error: {}", memtable_id, decode_error_to_message(&decode_error))
+            }
+            LsmError::CannotCreateManifest(io_error) => {
+                write!(f, "Cannot create manifest file. IO Error: {}", io_error)
+            }
+            LsmError::CannotWriteManifestOperation(_, io_error) => {
+                write!(f, "Cannot write manifest operation. IO Error: {}", io_error)
+            }
+            LsmError::CannotReadManifestOperations(io_error) => {
+                write!(f, "Cannot read manifest operations. IO Error: {}", io_error)
+            }
+            LsmError::CannotDecodeManifest(decode_error) => {
+                write!(f, "Cannot decode manifest. Error: {}", decode_error_to_message(&decode_error))
+            }
+            LsmError::CannotResetManifest(io_error) => {
+                write!(f, "Cannot clear manifest. Error: {}", io_error)
+            }
+            LsmError::CannotOpenSSTableFile(sstable_id, io_error) => {
+                write!(f, "Cannot open SSTable. SSTable ID: {}. Error: {}", sstable_id, io_error)
+            }
+            LsmError::CannotReadSSTableFile(sstable_id, io_error) => {
+                write!(f, "Cannot read SSTable. SSTable ID: {}. Error: {}", sstable_id, io_error)
+            }
+            LsmError::CannotDecodeSSTable(sstable_id, error_part, decode_error) => {
+                write!(f, "{}", sstable_decode_error_to_message(*sstable_id, error_part.clone(), decode_error))
+            }
+            LsmError::CannotDeleteSSTable(sstable_id, io_error) => {
+                write!(f, "Cannot delete SSTable. SSTable ID: {} Error: {}", sstable_id, io_error)
+            }
+            LsmError::CannotCreateSSTableFile(sstable_id, io_error) => {
+                write!(f, "Cannot create SSTable file. SSTable ID: {} Error: {}", sstable_id, io_error)
+            }
+            LsmError::Internal => {
+                panic!("This error shoudnt be returned to the final user!! Invalid code path");
+            }
+        }
     }
+}
+
+fn sstable_decode_error_to_message(
+    sstable_id: usize,
+    corrupted_part: SSTableCorruptedPart,
+    decode_error_info: &DecodeErrorInfo
+) -> String {
+    let mut message = String::new();
+
+    let corrupted_part: String = match corrupted_part {
+        SSTableCorruptedPart::BlockMetadata => "block metadata".to_string(),
+        SSTableCorruptedPart::BloomFilter => "bloom filter".to_string(),
+        SSTableCorruptedPart::Block(block_id) => format!("block ID {}", block_id),
+    };
+
+    message.push_str(format!("Cannot decode SSTable {}. SSTable ID: {}. Error: {}", corrupted_part,
+                             sstable_id, decode_error_to_message(decode_error_info)).as_str());
+
+    message
+}
+
+fn decode_error_to_message(decode_error: &DecodeErrorInfo) -> String {
+    let mut message = String::new();
+
+    message.push_str(format!("File {} in file offset {} in index {}: ", decode_error.path.as_path().to_str().unwrap(),
+                         decode_error.offset, decode_error.index).as_str());
+
+    match &decode_error.error_type {
+        DecodeErrorType::CorruptedCrc(expected, actual) => {
+            message.push_str(format!("Corrupted CRC Expected {} Actual {}", expected, actual).as_str());
+        },
+        DecodeErrorType::Utf8Decode(utf8_error) => {
+            message.push_str(format!("Invalid UTF-8: {}", utf8_error).as_str());
+        },
+        DecodeErrorType::JsonSerdeDeserialization(serde_error) => {
+            message.push_str(format!("Invalid JSON format when deserializing: {}", serde_error).as_str());
+        },
+        DecodeErrorType::IllegalSize(expected, actual) => {
+            message.push_str(format!("Illegal size. Expected {}, Actual {}", expected, actual).as_str());
+        }
+        DecodeErrorType::UnknownFlag(unknown_flgag) => {
+            message.push_str(format!("Unknown flag {}", unknown_flgag).as_str());
+        },
+    }
+
+    message
 }
