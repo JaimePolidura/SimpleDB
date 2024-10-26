3c38c55f7323c88b47f487ed7f4aa64836fc4d55
Jaime Polidura <jaime.polidura@gmail.com>
2024-10-26 16:08:30 +0200

    Fixed bugs in sorter

--- db/src/sql/execution/sort/sort_page_run_iterator.rs
+++ db/src/sql/execution/sort/sort_page_run_iterator.rs
@@ -1,40 +1,27 @@
-use crate::sql::execution::sort::sort_page::SortPage;
-use crate::{Row, Schema};
-use shared::SimpleDbError::{CannotReadSortFile, CannotWriteSortFile};
-use shared::{SimpleDbError, SimpleDbFile, SimpleDbFileMode, SimpleDbOptions};
-use std::sync::Arc;
-use storage::TemporarySpace;
 use crate::sql::execution::sort::sort_file::SortFile;
 use crate::sql::execution::sort::sort_files::SortFilePageIteratorState::LeftAndRightAvailable;
-
-#[derive(Clone)]
-pub struct SortFiles {
-    temporary_space: TemporarySpace,
-    options: Arc<SimpleDbOptions>,
-    table_schema: Schema,
-    initialized: bool,
-
-    output: Option<SortFile>,
-    input: Option<SortFile>,
-}
+use crate::{Row, Schema};
+use shared::SimpleDbError;
 
 //This iterator will maintain two pointers on the file (left and right) that will be separated by k
-//When the pages returned by these pointers reaches K, they (left & right( will be advanced by k pages.
+//When the pages returned by these pointers reaches n_pages_per_run, they (left & right( will be advanced by n_pages_per_run pages.
 //This will be used by external merge sort algoritm (sorter.rs).
 //'a lives as long as SortFiles lives
 //k indicates the difference in sort pages that there will be in left & right offsets
 //https://www.youtube.com/watch?v=F9XmmS8rL4c&t=698s
-pub struct SortFilePageIterator<'a> {
+pub struct SortPageRunIterator {
     table_schema: Schema,
 
     sort_page_size_bytes: usize,
-    file: &'a SortFile,
-    k: usize,
+    file: SortFile,
+    n_pages_to_return_per_run: usize,
 
     state: SortFilePageIteratorState,
     current_offset_left: usize,
     current_offset_right: usize,
-    n_pages_returned_right: usize,
+    n_pages_returned_right_in_run: usize,
+    n_pages_returned_left_in_run: usize,
+    n_current_run: usize,
 }
 
 enum SortFilePageIteratorState {
@@ -42,93 +29,24 @@ enum SortFilePageIteratorState {
     OnlyLeftAvailable
 }
 
-impl SortFiles {
+impl SortPageRunIterator {
     pub fn create(
-        temporary_space: TemporarySpace,
-        options: Arc<SimpleDbOptions>,
-        table_schema: Schema
-    ) -> Result<SortFiles, SimpleDbError> {
-        Ok(SortFiles {
-            initialized: false,
-            temporary_space,
-            output: None,
-            table_schema,
-            input: None,
-            options
-        })
-    }
-
-    pub fn input_iterator(&self, k: usize) -> Result<SortFilePageIterator, SimpleDbError> {
-        SortFilePageIterator::create(
-            self.input.as_ref().unwrap(),
-            self.options.sort_page_size_bytes,
-            &self.table_schema,
-            k
-        )
-    }
-
-    pub fn take_output_file(&mut self) -> SortFile {
-        self.output.take().unwrap()
-    }
-
-    pub fn swap_input_output_files(&mut self) {
-        let output = self.output.take().unwrap();
-        let input = self.input.take().unwrap();
-        self.input = Some(output);
-        self.output = Some(input);
-    }
-
-    pub fn write_sort_page_to_output(&mut self, page: SortPage) -> Result<(), SimpleDbError> {
-        self.maybe_initialize()?;
-
-        self.output
-            .as_mut()
-            .unwrap()
-            .write(page)
-    }
-
-    pub fn write_sort_page_to_input(&mut self, page: SortPage) -> Result<(), SimpleDbError> {
-        self.maybe_initialize()?;
-
-        self.input
-            .as_mut()
-            .unwrap()
-            .write(page)
-    }
-
-    fn maybe_initialize(&mut self) -> Result<(), SimpleDbError> {
-        if !self.initialized {
-            self.output = Some(SortFile::create(
-                self.temporary_space.create_file("output", SimpleDbFileMode::AppendOnly)?,
-                self.options.sort_page_size_bytes)
-            );
-            self.input = Some(SortFile::create(
-                self.temporary_space.create_file("input", SimpleDbFileMode::AppendOnly)?,
-                self.options.sort_page_size_bytes)
-            );
-            self.initialized = false;
-        }
-
-        Ok(())
-    }
-}
-
-impl<'a> SortFilePageIterator<'a> {
-    pub fn create(
-        file: &'a SortFile,
+        file: SortFile,
         sort_page_size_bytes: usize,
         schema: &Schema,
         k: usize,
-    ) -> Result<SortFilePageIterator<'a>, SimpleDbError> {
-        let mut iterator = SortFilePageIterator {
+    ) -> Result<SortPageRunIterator, SimpleDbError> {
+        let mut iterator = SortPageRunIterator {
             state: LeftAndRightAvailable,
             table_schema: schema.clone(),
+            n_pages_returned_right_in_run: 0,
+            n_pages_returned_left_in_run: 0,
             current_offset_right: 0, //Will get init when calling next_right()
-            n_pages_returned_right: 0,
             current_offset_left: 0,
             sort_page_size_bytes,
+            n_pages_to_return_per_run: k,
+            n_current_run: 0,
             file,
-            k,
         };
 
         iterator.initialize()?;
@@ -136,11 +54,18 @@ impl<'a> SortFilePageIterator<'a> {
         Ok(iterator)
     }
 
-    //Expect next_left() and next_right() to be called at the same time
     pub fn next_left(&mut self) -> Result<Option<Vec<Row>>, SimpleDbError> {
+        self.maybe_go_to_next_run()?;
+
+        //The user should call next_right
+        if self.n_pages_returned_left_in_run == self.n_pages_to_return_per_run {
+            return Ok(Some(Vec::new()));
+        }
+
         match self.read_row(self.current_offset_left)? {
             Some((row, new_offset)) => {
                 self.current_offset_left = new_offset;
+                self.n_pages_returned_left_in_run += 1;
                 Ok(Some(row))
             }
             None => Ok(None)
@@ -148,17 +73,21 @@ impl<'a> SortFilePageIterator<'a> {
     }
 
     //Expect next_left() and next_right() to be called at the same time
-    //Expect call next_right() after next_left()
     pub fn next_right(&mut self) -> Result<Option<Vec<Row>>, SimpleDbError> {
-        self.maybe_move_left_and_right_offsets()?;
+        self.maybe_go_to_next_run()?;
         self.maybe_initialize_right_offset()?;
+
+        //The user should call next_left
+        if self.n_pages_returned_right_in_run == self.n_pages_to_return_per_run {
+            return Ok(Some(Vec::new()));
+        }
 
         match self.state {
             SortFilePageIteratorState::LeftAndRightAvailable => {
                 match self.read_row(self.current_offset_right)? {
                     Some((row, new_offset)) => {
                         self.current_offset_right = new_offset;
-                        self.n_pages_returned_right += 1;
+                        self.n_pages_returned_right_in_run += 1;
                         Ok(Some(row))
                     }
                     None => Ok(None)
@@ -171,16 +100,27 @@ impl<'a> SortFilePageIterator<'a> {
     }
 
     pub fn has_next(&self) -> bool {
-        self.current_offset_left < self.file.size() || self.current_offset_right < self.file.size()
+        match self.state {
+            SortFilePageIteratorState::LeftAndRightAvailable => {
+                self.current_offset_left < self.file.size() || self.current_offset_right < self.file.size()
+            },
+            SortFilePageIteratorState::OnlyLeftAvailable => {
+                self.current_offset_left < self.file.size()
+            }
+        }
     }
 
-    fn maybe_move_left_and_right_offsets(&mut self) -> Result<(), SimpleDbError> {
-        if self.n_pages_returned_right == self.k {
-            self.n_pages_returned_right = 0;
+    fn maybe_go_to_next_run(&mut self) -> Result<(), SimpleDbError> {
+        if self.n_pages_returned_right_in_run == self.n_pages_to_return_per_run &&
+            self.n_pages_returned_left_in_run == self.n_pages_to_return_per_run &&
+            matches!(self.state, SortFilePageIteratorState::LeftAndRightAvailable) {
+
             self.current_offset_left = self.current_offset_right;
+            self.n_pages_returned_right_in_run = 0;
+            self.n_pages_returned_left_in_run = 0;
 
             match self.file.get_next_page_offset(
-                self.current_offset_right + (self.sort_page_size_bytes * self.k)
+                self.current_offset_right + (self.sort_page_size_bytes * self.n_pages_to_return_per_run)
             )? {
                 Some(new_right_offset) => {
                     self.state = SortFilePageIteratorState::LeftAndRightAvailable;
@@ -208,7 +148,7 @@ impl<'a> SortFilePageIterator<'a> {
     }
 
     fn initialize(&mut self) -> Result<(), SimpleDbError> {
-        match self.file.get_next_page_offset(self.current_offset_left * self.k)? {
+        match self.file.get_next_page_offset(self.current_offset_left * self.n_pages_to_return_per_run)? {
             Some(right_offset) => {
                 self.state = SortFilePageIteratorState::LeftAndRightAvailable;
                 self.current_offset_right = right_offset;
@@ -227,13 +167,13 @@ impl<'a> SortFilePageIterator<'a> {
             return Ok(());
         }
         //File too small
-        if self.k * self.sort_page_size_bytes >= self.file.size() {
+        if self.n_pages_to_return_per_run * self.sort_page_size_bytes >= self.file.size() {
             self.state = SortFilePageIteratorState::OnlyLeftAvailable;
             return Ok(());
         }
 
         //Right offset always point to the beggining of a page
-        let start_right_offset = self.k * self.sort_page_size_bytes;
+        let start_right_offset = self.n_pages_to_return_per_run * self.sort_page_size_bytes;
 
         match self.file.get_next_page_offset(start_right_offset)? {
             Some(new_offset) => {
